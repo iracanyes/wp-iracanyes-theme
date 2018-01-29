64d786743bd81d7767b5423716868d605fd7a08f
iracanyes <k.iracanye@gmail.com>
2018-01-29 01:59:19 +0100

    Découpage des templates

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 <h1>Wp Iracanyes Theme</h1>
+<div>
+ <ul>
+  <li>
+   Tâches effectués:
+  </li>
+  <li>
+   A faire:
+    <ul>
+     <li>
+      Exporter les scripts complémentaires dans le template home.php et front-page.php
+     </li>
+    </ul>
+  </li>
+ </ul>
+</div>
+
---
+++ home.php
@@ -0,0 +1,339 @@
+<?php
+/**
+ * Description: Template pour 2 types de pages
+ *  Page d'accueil par défaut
+ *  Liste d'article non-triée
+ * Dans ce tempate, il sera utilisé pour la page "Actualité"
+ */
+
+// Ajout du header de la page
+get_header();
+?>
+
+
+        <section id="subheader" data-speed="8" data-type="background" class="padding-top-bottom subheader">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-12">
+                        <h1>Blog List</h1>
+                        <ul class="breadcrumbs">
+                            <li><a href="index.html">Home</a></li> 
+                            <b>/</b> 
+                            <li class="active">Blog List</li>
+                        </ul>            
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <!-- content begin -->
+        <div id="content">
+            <div class="container">
+                <div class="row"> 
+                    <div id="blog-grid" class="blog-grid">
+                        <!-- post begin -->
+                        <article class="item col-md-4 col-sm-6">
+                            <div class="post-media">
+                                <img  alt="" src="images/news/2.jpg" class="img-responsive">
+                                <div class="post-date">
+                                    <span class="date-day">22</span>
+                                    <span class="date-month">May</span>
+                                </div>
+                            </div>
+                            <div class="post-content">
+                                <div class="post-title">
+                                    <h5><a href="article.html">Help You Get Amazing Theme</a></h5>
+                                </div>
+                                <div class="post-metadata">
+                                    <span class="byline">
+                                        <i class="fa fa-user"></i>
+                                        <a href="#">AuThemes</a>
+                                    </span>
+                                    <span class="cat-links">                                            
+                                        <i class="fa fa-folder-open"></i>
+                                        <a href="#">Markup</a>
+                                    </span>
+                                       
+                                </div>
+                                <div class="post-entry">
+                                    <p>In varius varius justo, eget ultrices mauris rhoncus non. Morbi tristique, mauris eu imperdiet bibendum</p>
+                                    
+                                </div>
+                            </div>
+                        </article>
+                        <!-- post close -->
+
+                        <!-- post begin -->
+                        <article class="item col-md-4 col-sm-6">
+                            <div class="post-media">
+                                <img  alt="" src="images/news/3.jpg" class="img-responsive">
+                                <div class="post-date">
+                                    <span class="date-day">19</span>
+                                    <span class="date-month">May</span>
+                                </div>
+                            </div>
+                            <div class="post-content">
+                                <div class="post-title">
+                                    <h5><a href="article.html">Why Designers Need Marketing Skills</a></h5>
+                                </div>
+                                <div class="post-metadata">
+                                    <span class="byline">
+                                        <i class="fa fa-user"></i>
+                                        <a href="#">AuThemes</a>
+                                    </span>
+                                    <span class="cat-links">                                            
+                                        <i class="fa fa-folder-open"></i>
+                                        <a href="#">Markup</a>
+                                    </span>                                       
+                                </div>
+                                <div class="post-entry">
+                                    <p>In varius varius justo, eget ultrices mauris rhoncus non. Morbi tristique, mauris eu imperdiet bibendum</p>
+                                    
+                                </div>
+                            </div>
+                        </article>
+                        <!-- post close -->
+
+                        <!-- post begin -->
+                        <article class="item col-md-4 col-sm-6">
+                            <div class="post-media">
+                                <img  alt="" src="images/news/4.jpg" class="img-responsive">
+                                <div class="post-date">
+                                    <span class="date-day">18</span>
+                                    <span class="date-month">May</span>
+                                </div>
+                            </div>
+                            <div class="post-content">
+                                <div class="post-title">
+                                    <h5><a href="article.html">10 Ways To Design For The Human Brain</a></h5>
+                                </div>
+                                <div class="post-metadata">
+                                    <span class="byline">
+                                        <i class="fa fa-user"></i>
+                                        <a href="#">AuThemes</a>
+                                    </span>
+                                    <span class="cat-links">                                            
+                                        <i class="fa fa-folder-open"></i>
+                                        <a href="#">Markup</a>
+                                    </span>                                       
+                                </div>
+                                <div class="post-entry">
+                                    <p>In varius varius justo, eget ultrices mauris rhoncus non. Morbi tristique, mauris eu imperdiet bibendum</p>
+                                    
+                                </div>
+                            </div>
+                        </article>
+                        <!-- post close -->
+
+                        <!-- post begin -->
+                        <article class="item col-md-4 col-sm-6">
+                            <div class="post-media">
+                                <img  alt="" src="images/news/5.jpg" class="img-responsive">
+                                <div class="post-date">
+                                    <span class="date-day">15</span>
+                                    <span class="date-month">May</span>
+                                </div>
+                            </div>
+                            <div class="post-content">
+                                <div class="post-title">
+                                    <h5><a href="article.html">UX Design Trends For 2016</a></h5>
+                                </div>
+                                <div class="post-metadata">
+                                    <span class="byline">
+                                        <i class="fa fa-user"></i>
+                                        <a href="#">AuThemes</a>
+                                    </span>
+                                    <span class="cat-links">                                            
+                                        <i class="fa fa-folder-open"></i>
+                                        <a href="#">Markup</a>
+                                    </span>
+                                       
+                                </div>
+                                <div class="post-entry">
+                                    <p>In varius varius justo, eget ultrices mauris rhoncus non. Morbi tristique, mauris eu imperdiet bibendum</p>
+                                    
+                                </div>
+                            </div>
+                        </article>
+                        <!-- post close -->
+
+                        <!-- post begin -->
+                        <article class="item col-md-4 col-sm-6">
+                            <div class="post-media">
+                                <img  alt="" src="images/news/6.jpg" class="img-responsive">
+                                <div class="post-date">
+                                    <span class="date-day">12</span>
+                                    <span class="date-month">May</span>
+                                </div>
+                            </div>
+                            <div class="post-content">
+                                <div class="post-title">
+                                    <h5><a href="article.html">Mobile UX And User Expectations</a></h5>
+                                </div>
+                                <div class="post-metadata">
+                                    <span class="byline">
+                                        <i class="fa fa-user"></i>
+                                        <a href="#">AuThemes</a>
+                                    </span>
+                                    <span class="cat-links">                                            
+                                        <i class="fa fa-folder-open"></i>
+                                        <a href="#">Markup</a>
+                                    </span>
+                                       
+                                </div>
+                                <div class="post-entry">
+                                    <p>In varius varius justo, eget ultrices mauris rhoncus non. Morbi tristique, mauris eu imperdiet bibendum</p>
+                                    
+                                </div>
+                            </div>
+                        </article>
+                        <!-- post close -->
+                        <!-- post begin -->
+                        <article class="item col-md-4 col-sm-6">
+                            <div class="post-media">
+                                <img  alt="" src="images/news/7.jpg" class="img-responsive">
+                                <div class="post-date">
+                                    <span class="date-day">12</span>
+                                    <span class="date-month">May</span>
+                                </div>
+                            </div>
+                            <div class="post-content">
+                                <div class="post-title">
+                                    <h5><a href="article.html">Mobile UX And User Expectations</a></h5>
+                                </div>
+                                <div class="post-metadata">
+                                    <span class="byline">
+                                        <i class="fa fa-user"></i>
+                                        <a href="#">AuThemes</a>
+                                    </span>
+                                    <span class="cat-links">                                            
+                                        <i class="fa fa-folder-open"></i>
+                                        <a href="#">Markup</a>
+                                    </span>                                       
+                                </div>
+                                <div class="post-entry">
+                                    <p>In varius varius justo, eget ultrices mauris rhoncus non. Morbi tristique, mauris eu imperdiet bibendum</p>                                    
+                                </div>
+                            </div>
+                        </article>
+                        <!-- post close -->
+                        <!-- post begin -->
+                        <article class="item col-md-4 col-sm-6">
+                            <div class="post-media">
+                                <img alt="" src="images/news/8.jpg" class="img-responsive">
+                                <div class="post-date">
+                                    <span class="date-day">12</span>
+                                    <span class="date-month">May</span>
+                                </div>
+                            </div>
+                            <div class="post-content">
+                                <div class="post-title">
+                                    <h5><a href="article.html">Mobile UX And User Expectations</a></h5>
+                                </div>
+                                <div class="post-metadata">
+                                    <span class="byline">
+                                        <i class="fa fa-user"></i>
+                                        <a href="#">AuThemes</a>
+                                    </span>
+                                    <span class="cat-links">                                            
+                                        <i class="fa fa-folder-open"></i>
+                                        <a href="#">Markup</a>
+                                    </span>
+                                       
+                                </div>
+                                <div class="post-entry">
+                                    <p>In varius varius justo, eget ultrices mauris rhoncus non. Morbi tristique, mauris eu imperdiet bibendum</p>
+                                    
+                                </div>
+                            </div>
+                        </article>
+                        <!-- post close -->
+                        <!-- post begin -->
+                        <article class="item col-md-4 col-sm-6">
+                            <div class="post-media">
+                                <img alt="" src="images/news/9.jpg" class="img-responsive">
+                                <div class="post-date">
+                                    <span class="date-day">12</span>
+                                    <span class="date-month">May</span>
+                                </div>
+                            </div>
+                            <div class="post-content">
+                                <div class="post-title">
+                                    <h5><a href="article.html">Mobile UX And User Expectations</a></h5>
+                                </div>
+                                <div class="post-metadata">
+                                    <span class="byline">
+                                        <i class="fa fa-user"></i>
+                                        <a href="#">AuThemes</a>
+                                    </span>
+                                    <span class="cat-links">                                            
+                                        <i class="fa fa-folder-open"></i>
+                                        <a href="#">Markup</a>
+                                    </span>
+                                       
+                                </div>
+                                <div class="post-entry">
+                                    <p>In varius varius justo, eget ultrices mauris rhoncus non. Morbi tristique, mauris eu imperdiet bibendum</p>
+                                    
+                                </div>
+                            </div>
+                        </article>
+                        <!-- post close -->
+                        <!-- post begin -->
+                        <article class="item col-md-4 col-sm-6">
+                            <div class="post-media">
+                                <img alt="" src="images/news/1.jpg" class="img-responsive">
+                                <div class="post-date">
+                                    <span class="date-day">12</span>
+                                    <span class="date-month">May</span>
+                                </div>
+                            </div>
+                            <div class="post-content">
+                                <div class="post-title">
+                                    <h5><a href="article.html">Mobile UX And User Expectations</a></h5>
+                                </div>
+                                <div class="post-metadata">
+                                    <span class="byline">
+                                        <i class="fa fa-user"></i>
+                                        <a href="#">AuThemes</a>
+                                    </span>
+                                    <span class="cat-links">                                            
+                                        <i class="fa fa-folder-open"></i>
+                                        <a href="#">Markup</a>
+                                    </span>
+                                       
+                                </div>
+                                <div class="post-entry">
+                                    <p>In varius varius justo, eget ultrices mauris rhoncus non. Morbi tristique, mauris eu imperdiet bibendum</p>
+                                    
+                                </div>
+                            </div>
+                        </article>
+                        <!-- post close -->
+                    </div>
+
+                    <!-- pagination begin -->
+                    <div class="pagination-ourter text-center">
+                        <ul class="pagination">
+                            <li><a class="prev page-numbers" href="#"><i class="fa fa-angle-left"></i></a></li>
+                            <li><a class="next page-numbers" href="#"><i class="fa fa-angle-right"></i></a></li>
+                        </ul>
+                    </div>
+                    <!-- pagination close -->
+                             
+                </div>
+            </div> 
+        </div>
+        <!-- content close -->
+
+
+
+
+    <!-- Waypoints-->
+
+
+
+<?php
+// Ajout du footer de la page
+get_footer();
+?>
